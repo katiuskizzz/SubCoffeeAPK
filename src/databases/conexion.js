bca27b318931b8228844f5412991fea80c95c8ef
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-06-12 10:26:56 -0500

    Parte dela conexion

--- src/databases/conexion.js
+++ src/databases/conexion.js
@@ -10,11 +10,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../../src/env/.env") });
 
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABSE
+    host: 'viaduct.proxy.rlwy.net',
+    user: 'root',
+    password: 'wLyRnZkgvjYsBpkxqVoOzTXKdoyBMwwm',
+    port: 26817,
+    database: 'railway'
 })
 
 //Validar conexión a la base de datos
