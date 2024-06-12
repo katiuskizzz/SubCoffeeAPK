0668421b8019150aa07ee6bff4bb58836b5802e5
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-06-12 14:44:50 -0500

    AS

--- src/databases/conexion.js
+++ src/databases/conexion.js
@@ -1,30 +1,25 @@
 import { createPool } from "mysql2/promise";
-/* import dotenv from "dotenv";
- */
-/* dotenv.config({ path: "./src/env/.env" }); */
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
 
+// Obtener la ruta del archivo actual 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../src/env/.env") });
 export const pool = createPool({
-  host: 'viaduct.proxy.rlwy.net',
-  user: 'root',
-  password: 'wLyRnZkgvjYsBpkxqVoOzTXKdoyBMwwm',
-  port: 26817,
-  database: 'railway',
-});
+    host: 'viaduct.proxy.rlwy.net',
+    user: 'root',
+    password: 'wLyRnZkgvjYsBpkxqVoOzTXKdoyBMwwm',
+    port: 26817,
+    database: 'railway'
+})
 
-pool
-  .getConnection()
-  .then((connect) => {
-    console.log("Conexión a la database coffeeoffer exitosa");
+pool.getConnection().then(connect => {
+    console.log("Conexión a base de datos exitosa.");
     connect.release();
-  })
-  .catch((error) => {
-    console.log("Conexión a la database fallida " + error);
-  });
-
-// export const pool = createPool({
-//     host: 'viaduct.proxy.rlwy.net',
-//     user: 'root',
-//     password: 'wLyRnZkgvjYsBpkxqVoOzTXKdoyBMwwm',
-//     port: 26817,
-//     database: 'railway'
-// })
+})
+    .catch(error => {
+        console.error("Conexion a base de datos fallida. " + error);
+})  
