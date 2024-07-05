06f0d2e4c43dc02ec27594bf5df1c138ef286573
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-07-05 16:56:21 -0500

    Solucion token

--- src/routes/subasta.routes.js
+++ src/routes/subasta.routes.js
@@ -22,7 +22,7 @@ import { verificarUserToken } from "../controllers/autenticacionController.js";
 const routerSubasta = Router();
 
 routerSubasta.post("/subasta", subastaFiles, verificarUserToken, registrar);
-routerSubasta.get("/subasta", verificarUserToken, listar);
+routerSubasta.get("/subasta", listar);
 routerSubasta.put("/subasta/:id", verificarUserToken, subastaFiles, actualizar);
 routerSubasta.get("/buscar/:id", verificarUserToken, buscar); 
 routerSubasta.delete("/eliminar/:id", verificarUserToken, eliminar);
