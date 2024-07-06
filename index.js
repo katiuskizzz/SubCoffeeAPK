c91145a68ed2dcee67b3f3731961ae9f516eaef9
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-07-06 12:22:21 -0500

    Verificacion de la imagen

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use("/auth", autenticacionRouter);
 app.use("/v1", routerUser);
